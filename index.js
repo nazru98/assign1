546781ed6dc4de33876a07006937cd7100f38f46
Nazruddin Ahmad <nazruddin.ahmad48@gmail.com>
2023-09-29 16:46:13 +0530

    Employee Server

--- index.js
+++ index.js
@@ -205,601 +205,7 @@ app.get("/totalPurchase1/product1/:id", function (req, res) {
 });
 
 
-
-
-
-
-const pageSize = 5;
-let id = 745;
-let persons = [
-  {
-    id: "1",
-    name: "Jack Smith",
-    age: 28,
-    city: "London",
-    company: "Apple",
-  },
-  {
-    id: "2",
-    name: "Bob Kelley",
-    age: 37,
-    city: "Paris",
-    company: "Microsoft",
-  },
-  {
-    id: "3",
-    name: "Amit Gupta",
-    age: 29,
-    city: "New Delhi",
-    company: "Google",
-  },
-  {
-    id: "6",
-    name: "Mary Stevens",
-    age: 30,
-    city: "London",
-    company: "Facebook",
-  },
-  {
-    id: "8",
-    name: "Dave Burton",
-    age: 31,
-    city: "Paris",
-    company: "Tesla",
-  },
-  {
-    id: "11",
-    name: "Pradeep Kumar",
-    age: 43,
-    city: "Bangalore",
-    company: "Amazon",
-  },
-  {
-    id: "16",
-    name: "Tim Jonas",
-    age: 47,
-    city: "London",
-    company: "Microsoft",
-  },
-  {
-    id: "18",
-    name: "Julia Martins",
-    age: 34,
-    city: "Paris",
-    company: "Apple",
-  },
-  {
-    id: "21",
-    name: "Payal Sethi Gupta",
-    age: 41,
-    city: "New Delhi",
-    company: "Microsoft",
-  },
-  {
-    id: "25",
-    name: "Anita Burton",
-    age: 34,
-    city: "London",
-    company: "Google",
-  },
-  {
-    id: "34",
-    name: "George Matthews",
-    age: 26,
-    city: "London",
-    company: "Tesla",
-  },
-  {
-    id: "28",
-    name: "Vish Talwar",
-    age: 46,
-    city: "New Delhi",
-    company: "Amazon",
-  },
-  {
-    id: "41",
-    name: "Pooja Kaur",
-    age: 25,
-    city: "Bangalore",
-    company: "Amazon",
-  },
-  {
-    id: "51",
-    name: "John Bundy",
-    age: 27,
-    city: "London",
-    company: "Amazon",
-  },
-  {
-    id: "52",
-    name: "Anna Matthews",
-    age: 39,
-    city: "Paris",
-    company: "Google",
-  },
-  {
-    id: "53",
-    name: "Pankaj Gupta",
-    age: 32,
-    city: "New Delhi",
-    company: "Facebook",
-  },
-  {
-    id: "56",
-    name: "Kathy Graham",
-    age: 34,
-    city: "London",
-    company: "Tesla",
-  },
-  {
-    id: "58",
-    name: "Tony Fullerton",
-    age: 36,
-    city: "Paris",
-    company: "Amazon",
-  },
-  {
-    id: "61",
-    name: "Smita Kumar",
-    age: 38,
-    city: "Bangalore",
-    company: "Microsoft",
-  },
-  {
-    id: "76",
-    name: "Harry Smith",
-    age: 41,
-    city: "London",
-    company: "Apple",
-  },
-  {
-    id: "78",
-    name: "Amy Martins",
-    age: 27,
-    city: "Paris",
-    company: "Microsoft",
-  },
-  {
-    id: "81",
-    name: "Richa Singh",
-    age: 33,
-    city: "New Delhi",
-    company: "Google",
-  },
-  {
-    id: "95",
-    name: "Boris Thompson",
-    age: 43,
-    city: "London",
-    company: "Tesla",
-  },
-  {
-    id: "94",
-    name: "John Major",
-    age: 36,
-    city: "London",
-    company: "Amazon",
-  },
-  {
-    id: "88",
-    name: "Ashish Talwar",
-    age: 57,
-    city: "New Delhi",
-    company: "Microsoft",
-  },
-  {
-    id: "121",
-    name: "T Ashwin",
-    age: 40,
-    city: "Bangalore",
-    company: "Google",
-  },
-  {
-    id: "145",
-    name: "Steve Paine",
-    age: 31,
-    city: "London",
-    company: "Apple",
-  },
-  {
-    id: "167",
-    name: "David Cummins",
-    age: 42,
-    city: "Paris",
-    company: "Microsoft",
-  },
-  {
-    id: "172",
-    name: "Rishabh Gupta",
-    age: 34,
-    city: "New Delhi",
-    company: "Google",
-  },
-  {
-    id: "176",
-    name: "Serena Stevens",
-    age: 25,
-    city: "London",
-    company: "Facebook",
-  },
-  {
-    id: "184",
-    name: "Lionel Burton",
-    age: 27,
-    city: "Paris",
-    company: "Tesla",
-  },
-  {
-    id: "211",
-    name: "Navdeep Kumar",
-    age: 40,
-    city: "Bangalore",
-    company: "Amazon",
-  },
-  {
-    id: "216",
-    name: "Joe Harris",
-    age: 45,
-    city: "London",
-    company: "Microsoft",
-  },
-  {
-    id: "218",
-    name: "Angela Smith",
-    age: 33,
-    city: "Paris",
-    company: "Apple",
-  },
-  {
-    id: "321",
-    name: "Neha Sehgal",
-    age: 41,
-    city: "New Delhi",
-    company: "Microsoft",
-  },
-  {
-    id: "225",
-    name: "Margaret B",
-    age: 35,
-    city: "London",
-    company: "Google",
-  },
-  {
-    id: "234",
-    name: "Harry Seldon",
-    age: 28,
-    city: "London",
-    company: "Tesla",
-  },
-  {
-    id: "428",
-    name: "Maya Iyer",
-    age: 49,
-    city: "New Delhi",
-    company: "Amazon",
-  },
-  {
-    id: "441",
-    name: "Anita Sood",
-    age: 29,
-    city: "Bangalore",
-    company: "Amazon",
-  },
-  {
-    id: "151",
-    name: "Donald Jr.",
-    age: 32,
-    city: "London",
-    company: "Amazon",
-  },
-  {
-    id: "252",
-    name: "Timothy Matthews",
-    age: 45,
-    city: "Paris",
-    company: "Google",
-  },
-  {
-    id: "435",
-    name: "Umesh Gupta",
-    age: 26,
-    city: "New Delhi",
-    company: "Facebook",
-  },
-  {
-    id: "286",
-    name: "Stephanie Graham",
-    age: 29,
-    city: "London",
-    company: "Tesla",
-  },
-  {
-    id: "158",
-    name: "Charles Bush",
-    age: 32,
-    city: "Paris",
-    company: "Amazon",
-  },
-  {
-    id: "261",
-    name: "Sonia Aiyer",
-    age: 35,
-    city: "Bangalore",
-    company: "Microsoft",
-  },
-  {
-    id: "576",
-    name: "Edwards Smith",
-    age: 39,
-    city: "London",
-    company: "Apple",
-  },
-  {
-    id: "378",
-    name: "Ken Rosewel",
-    age: 26,
-    city: "Paris",
-    company: "Microsoft",
-  },
-  {
-    id: "281",
-    name: "Rohit Jain",
-    age: 33,
-    city: "New Delhi",
-    company: "Google",
-  },
-  {
-    id: "295",
-    name: "Michael Fox",
-    age: 44,
-    city: "London",
-    company: "Tesla",
-  },
-  {
-    id: "194",
-    name: "Viktor Major",
-    age: 38,
-    city: "London",
-    company: "Amazon",
-  },
-  {
-    id: "688",
-    name: "Joy Sharma",
-    age: 54,
-    city: "New Delhi",
-    company: "Microsoft",
-  },
-  {
-    id: "721",
-    name: "Pranay Bansal",
-    age: 37,
-    city: "Bangalore",
-    company: "Google",
-  },
-];
-let products = [
-  {
-    id: "A101",
-    name: "Pepsi 300ml",
-    price: 20,
-  },
-  {
-    id: "A232",
-    name: "Diet Coke 300ml",
-    price: 25,
-  },
-  {
-    id: "A102",
-    name: "Pepsi 500ml",
-    price: 40,
-  },
-  {
-    id: "A237",
-    name: "Coke 1l",
-    price: 75,
-  },
-  {
-    id: "B034",
-    name: "Fruit and Nuts - 40g",
-    price: 15,
-  },
-  {
-    id: "B035",
-    name: "Crackles - 100g",
-    price: 45,
-  },
-  {
-    id: "B036",
-    name: "Nutties - 20g",
-    price: 10,
-  },
-  {
-    id: "B173",
-    name: "25gm bar",
-    price: 35,
-  },
-];
-let users = [
-  { username: "Emp101", password: "Emp101", name: "Jack Smith", role: "user" },
-  { username: "Emp107", password: "Emp107", name: "Mary Gomes", role: "user" },
-  { username: "Emp211", password: "Emp211", name: "Anna Steve", role: "admin" },
-  { username: "Emp218", password: "Emp218", name: "Bob Jenner", role: "admin" },
-];
-
-app.get("/productApp/products", function (req, res) {
-  res.send(products);
-});
-app.get("/productApp/users", function (req, res) {
-  let users1 = users.map((u) => ({
-    username: u.username,
-    name: u.name,
-    role: u.role,
-  }));
-  res.send(users1);
-});
-app.post("/productApp/products", (req, res) => {
-  const product = req.body;
-  let prod = products.find((pr) => pr.id === product.id);
-  if (!prod) {
-    products.push(product);
-    console.log(product);
-    res.send(product);
-  } else res.status(400).send("Product Id already exists");
-});
-app.post("/productApp/users", (req, res) => {
-  const user = req.body;
-  let u1 = users.find((u) => u.username === user.username);
-  if (!u1) {
-    users.push(user);
-    let user1 = {
-      username: user.username,
-      name: user.name,
-      role: user.role,
-    };
-    res.send(user1);
-  } else res.status(400).send("Username already exists");
-});
-app.post("/productApp/login", (req, res) => {
-  const details = req.body;
-  let user = users.find(
-    (u1) => u1.username === details.username && u1.password === details.password
-  );
-  if (user) {
-    let u1 = {
-      username: user.username,
-      name: user.name,
-      role: user.role,
-    };
-    res.send(u1);
-  } else res.status(400).send("Invalid username or password");
-});
-app.get("/productApp/products/:id", function (req, res) {
-  let id = req.params.id;
-  let obj = products.find((obj1) => obj1.id === id);
-  obj ? res.send(obj) : res.send("not found");
-});
-app.put("/productApp/products/:id", function (req, res) {
-  let id = req.params.id;
-  const product = req.body;
-  let index = products.findIndex((obj1) => obj1.id === id);
-  if (index >= 0) {
-    products[index] = product;
-    res.send(product);
-  } else res.send("not found");
-});
-app.delete("/productApp/products/:id", function (req, res) {
-  let id = req.params.id;
-  let index = products.findIndex((obj1) => obj1.id === id);
-  if (index >= 0) {
-    let product = products.splice(index, 1);
-    res.send(product);
-  } else res.send("not found");
-});
-app.put("/productApp/users/:username", function (req, res) {
-  let username = req.params.username;
-  const user = req.body;
-  let index = users.findIndex((obj1) => obj1.username === username);
-  if (index >= 0) {
-    users[index] = user;
-    let user1 = {
-      username: user.username,
-      name: user.name,
-      role: user.role,
-    };
-    res.send(user1);
-  } else res.send("not found");
-});
-app.delete("/productApp/users/:username", function (req, res) {
-  let username = req.params.username;
-  let index = users.findIndex((obj1) => obj1.username === username);
-  if (index >= 0) {
-    let user = users.splice(index, 1);
-    res.send("User deleted");
-  } else res.send("not found");
-});
-
-app.get("/personApp/persons", function (req, res) {
-  let page = req.query.page ? +req.query.page : 1;
-  let city = req.query.city;
-  let company = req.query.company;
-  let data1 = persons;
-  data1 = filterParam(data1, "city", city);
-  data1 = filterParam(data1, "company", company);
-  res.send(makeData(page, pageSize, data1));
-});
-
-app.post("/personApp/persons", (req, res) => {
-  const person = req.body;
-  person.id = id + 1;
-  id = id + 1;
-  persons.push(person);
-  res.send(person);
-});
-
-let makeData = (pageNum, size, data1) => {
-  let startIndex = (pageNum - 1) * size;
-  let endIndex =
-    data1.length > startIndex + size - 1
-      ? startIndex + size - 1
-      : data1.length - 1;
-  let data2 = data1.filter(
-    (lt, index) => index >= startIndex && index <= endIndex
-  );
-  let dataFull = {
-    startIndex: data1.length > 0 ? startIndex + 1 : startIndex,
-    endIndex: data1.length > 0 ? endIndex + 1 : endIndex,
-    numOfItems: data1.length,
-    data: data2,
-  };
-  return dataFull;
-};
-
-let filterParam = (arr, name, values) => {
-  if (!values) return arr;
-  let valuesArr = values.split(",");
-  let arr1 = arr.filter((a1) => valuesArr.find((val) => val === a1[name]));
-  return arr1;
-};
-
-app.get("/personApp/persons/:id", function (req, res) {
-  let id = req.params.id;
-  let obj = persons.find((obj1) => obj1.id === id);
-  if (obj) res.send(obj);
-  res.send("not found");
-});
-
-app.get("/productApp/users/:username", function (req, res) {
-  let username = req.params.username;
-  let user = users.find((obj1) => obj1.username === username);
-  user ? res.send(user) : res.send("not found");
-});
-
-app.put("/personApp/persons/:id", function (req, res) {
-  console.log("Put called");
-  let id = req.params.id;
-  const person = req.body;
-  console.log(id, person);
-  let updatedPerson = { id: id, ...person };
-  let index = persons.findIndex((obj1) => obj1.id === id);
-  if (index >= 0) {
-    persons[index] = updatedPerson;
-    res.send(updatedPerson);
-  } else res.send("not found");
-});
-
-app.delete("/personApp/persons/:id", function (req, res) {
-  let id = req.params.id;
-  let index = persons.findIndex((obj1) => obj1.id === id);
-  if (index >= 0) {
-    let person = persons.splice(index, 1);
-    res.send(person);
-  }
-  res.send("not found");
-});
-
-
-
-var users1 = [
+var users = [
   {
     role: "ADMIN",
     name: "Edufect",
@@ -923,7 +329,7 @@ var users1 = [
 
 app.post("/empapp/loginuser", function (req, res) {
   let body = { email: req.body.email, password: req.body.password };
-  let obj = users1.find(
+  let obj = users.find(
     (obj) => obj.email === body.email && obj.password === body.password
   );
   if (obj === undefined) res.status(401).send("Login Failed!!");
@@ -933,7 +339,7 @@ app.post("/empapp/loginuser", function (req, res) {
 });
 app.get("/empapp/emps", function (req, res) {
   let dispArr = [];
-  users1.map((obj) => {
+  users.map((obj) => {
     let obj1 = { ...obj };
     delete obj1.details;
     delete obj1.contact;
@@ -961,8 +367,8 @@ app.get("/empapp/emps", function (req, res) {
   });
 });
 app.post("/empapp/emps", function (req, res) {
-  let body = { empuserid: users1.length + 1, ...req.body };
-  users1.push(body);
+  let body = { empuserid: users.length + 1, ...req.body };
+  users.push(body);
   let obj = { ...body };
   delete obj.password;
   res.send(obj);
@@ -970,26 +376,26 @@ app.post("/empapp/emps", function (req, res) {
 app.get("/empapp/empdept/:empuserid", function (req, res) {
   let empid = +req.params.empuserid;
   console.log(empid);
-  let body = users1.find((obj) => +obj.empuserid === empid);
+  let body = users.find((obj) => +obj.empuserid === empid);
   console.log(body);
   let obj = { ...body.details, empuserid: body.empuserid };
   res.send(obj);
 });
 app.post("/empapp/empdept/:empuserid", function (req, res) {
   let empid = +req.params.empuserid;
-  let ind = users1.findIndex((obj) => +obj.empuserid === empid);
+  let ind = users.findIndex((obj) => +obj.empuserid === empid);
   console.log(ind);
   let obj = { ...req.body };
   delete obj.empuserid;
-  let body = users1[ind];
+  let body = users[ind];
   let obj1 = { ...body, details: { ...obj } };
   console.log(obj1);
-  users1[ind] = obj1;
+  users[ind] = obj1;
   res.send(req.body);
 });
 app.get("/empapp/empcontact/:empuserid", function (req, res) {
   let empid = +req.params.empuserid;
-  let obj = users1.find((obj) => +obj.empuserid === empid);
+  let obj = users.find((obj) => +obj.empuserid === empid);
   let body = { ...obj.contact, empuserid: obj.empuserid };
   res.send(body);
 });
@@ -997,20 +403,20 @@ app.get("/empapp/empcontact/:empuserid", function (req, res) {
 
 app.post("/empapp/empcontact/:empuserid", function (req, res) {
   let empid = +req.params.empuserid;
-  let ind = users1.findIndex((obj) => +obj.empuserid === empid);
+  let ind = users.findIndex((obj) => +obj.empuserid === empid);
   console.log(ind);
   let obj = { ...req.body };
   delete obj.empuserid;
-  let body = users1[ind];
+  let body = users[ind];
   let obj1 = { ...body, contact: { ...obj } };
   console.log(obj1);
-  users1[ind] = obj1;
+  users[ind] = obj1;
   res.send(req.body);
 });
 app.get("/empapp/empbills/:empuserid", function (req, res) {
   let empid = +req.params.empuserid;
-  let ind = users1.findIndex((obj) => +obj.empuserid === empid);
-  let dispArr = users1[ind].bills;
+  let ind = users.findIndex((obj) => +obj.empuserid === empid);
+  let dispArr = users[ind].bills;
   let page = +req.query.page;
   page = isNaN(page) ? 1 : page;
   //have to add pagination
@@ -1033,15 +439,15 @@ app.get("/empapp/empbills/:empuserid", function (req, res) {
 });
 app.post("/empapp/empbills/:empuserid", function (req, res) {
   let empid = +req.params.empuserid;
-  let ind = users1.findIndex((obj) => +obj.empuserid === empid);
+  let ind = users.findIndex((obj) => +obj.empuserid === empid);
   console.log(ind);
-  let obj = users1[ind];
+  let obj = users[ind];
   if (obj.bills === undefined) {
     let billArr = [];
     let obj1 = { billid: 1, ...req.body };
     billArr.push(obj1);
     let obj2 = { ...obj, bills: billArr };
-    users1[ind] = obj2;
+    users[ind] = obj2;
   } else {
     let billArr = obj.bills;
     console.log(billArr);
@@ -1049,51 +455,51 @@ app.post("/empapp/empbills/:empuserid", function (req, res) {
     billArr.push(obj1);
     obj.bills = billArr;
     let obj2 = { ...obj };
-    users1[ind] = obj2;
+    users[ind] = obj2;
   }
-  console.log(users1[ind]);
+  console.log(users[ind]);
   res.send(req.body);
 });
 app.post("/empapp/hotelbill", function (req, res) {
   let body = req.body;
-  let ind = users1.findIndex((obj1) => +obj1.empuserid === +body.empuserid);
-  let billInd = users1[ind].bills.findIndex(
+  let ind = users.findIndex((obj1) => +obj1.empuserid === +body.empuserid);
+  let billInd = users[ind].bills.findIndex(
     (obj1) => +obj1.billid === +body.billid
   );
-  //console.log(users1[ind].bills[billInd])
+  //console.log(users[ind].bills[billInd])
   delete body.billid;
   delete body.empuserid;
-  let body1 = users1[ind].bills[billInd];
+  let body1 = users[ind].bills[billInd];
   //console.log(body, body1);
   let obj = { ...body1, ...body };
   //console.log(obj);
-  users1[ind].bills[billInd] = obj;
-  res.send(users1[ind]);
+  users[ind].bills[billInd] = obj;
+  res.send(users[ind]);
 });
 app.get("/empapp/hotelbill/:empid/:billid", function (req, res) {
   let empid = +req.params.empid;
   //console.log(empid);
-  let ind = users1.findIndex((obj1) => +obj1.empuserid === empid);
-  let ob1 = users1.find((obj1) => +obj1.empuserid === +empid);
+  let ind = users.findIndex((obj1) => +obj1.empuserid === empid);
+  let ob1 = users.find((obj1) => +obj1.empuserid === +empid);
   let billid = +req.params.billid;
   console.log(billid);
   let billInd = ob1.bills.findIndex((obj) => +obj.billid === billid);
   console.log(billInd);
-  let obj = users1[ind].bills[billInd];
+  let obj = users[ind].bills[billInd];
   let obj1 = { empuserid: empid, ...obj };
   console.log(obj1);
   res.send(obj1);
 });
 app.get("/empapp/travelbill/:empid/:billid", function (req, res) {
   let empid = +req.params.empid;
-  let ind = users1.findIndex((obj1) => +obj1.empuserid === empid);
-  let ob1 = users1.find((obj1) => +obj1.empuserid === empid);
+  let ind = users.findIndex((obj1) => +obj1.empuserid === empid);
+  let ob1 = users.find((obj1) => +obj1.empuserid === empid);
   let billid = +req.params.billid;
   console.log(ob1);
   //finding bill index
   let billInd = ob1.bills.findIndex((obj) => +obj.billid === billid);
   console.log(billInd);
-  let obj = users1[ind].bills[billInd];
+  let obj = users[ind].bills[billInd];
   let obj1 = { empuserid: empid, ...obj };
   console.log(obj1);
   res.send(obj1);
@@ -1101,17 +507,17 @@ app.get("/empapp/travelbill/:empid/:billid", function (req, res) {
 app.post("/empapp/travelbill", function (req, res) {
   let body = req.body;
   console.log(body,'body');
-  let ind = users1.findIndex((obj1) => +obj1.empuserid === +body.empuserid);
-  let billInd = users1[ind].bills.findIndex(
+  let ind = users.findIndex((obj1) => +obj1.empuserid === +body.empuserid);
+  let billInd = users[ind].bills.findIndex(
     (obj1) => +obj1.billid === +body.billid
   );
   console.log(billInd);
   delete body.billid;
   delete body.empuserid;
-  let body1 = users1[ind].bills[billInd];
+  let body1 = users[ind].bills[billInd];
   let obj = { ...body, ...body1 };
-  users1[ind].bills[billInd] = obj;
-  res.send(users1[ind]);
+  users[ind].bills[billInd] = obj;
+  res.send(users[ind]);
 });
 
 function pagination(obj, page) {
@@ -1121,3 +527,5 @@ function pagination(obj, page) {
 }
 
 app.listen(port, () => console.log(`Node app listening on port ${port}!`));
+
+
