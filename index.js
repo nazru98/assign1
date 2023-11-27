28c564bbc91600fdaa019d7c7570a40f4f4bec88
Nazruddin Ahmad <nazruddin.ahmad48@gmail.com>
2023-11-27 18:42:58 +0530

    Postman server deployed again

--- index.js
+++ index.js
@@ -2021,7 +2021,13 @@ res.send(classesArr)
 });
 
 
-
+app.post("/postClass",(req,res)=>{
+let newClass=req.body;
+newClass.classId=classes.length+1;
+classes.push(newClass);
+res.send(classes),
+console.log(newClass,'ghjk');
+})
 
 app.put("/postClass/:id",(req,res)=>{
 let classId=+req.params.id;
